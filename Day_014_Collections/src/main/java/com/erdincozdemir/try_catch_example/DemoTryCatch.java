64ab3a5f1bd684f10278b15bea104fe8a0703d84
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-03-29 11:08:22 +0300

    Commit

--- Day_014_Collections/src/main/java/com/erdincozdemir/try_catch_example/DemoTryCatch.java
+++ Day_014_Collections/src/main/java/com/erdincozdemir/try_catch_example/DemoTryCatch.java
@@ -3,8 +3,6 @@ package com.erdincozdemir.try_catch_example;
 public class DemoTryCatch {
     public static void main(String[] args) {
 
-        //sıfıra bölme hatası
-
       int x = 10;
       x += 5;
         System.out.println(x);
