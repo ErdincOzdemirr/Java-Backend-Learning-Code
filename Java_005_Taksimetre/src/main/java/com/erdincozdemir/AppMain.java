32fca85a0adb286dcbed0ddf43f68bc389102c03
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 17:21:21 +0300

    Java Taksimetre

--- Java_005_Taksimetre/src/main/java/com/erdincozdemir/AppMain.java
+++ Java_005_Taksimetre/src/main/java/com/erdincozdemir/AppMain.java
@@ -1,7 +1,29 @@
 package com.erdincozdemir;
 
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int km = 0;
+        double perKm = 2.2, toplamTutar;
+        int startPrice = 10;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter kilometers: ");
+
+        km = scanner.nextInt();
+
+        toplamTutar = perKm * km;
+
+        double toplam = toplamTutar + startPrice;
+
+        toplam = (toplam < 20) ? 20 : toplam;
+
+        System.out.println("Ucret : " + toplam);
+
+
+
     }
 }
