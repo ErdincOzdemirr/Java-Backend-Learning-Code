5f6035fae0f1f20f7bc1d1f96f53f36cc3139e01
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 07:58:05 +0300

    Last Code

--- JavaProject_002_NotOrtalamasi2/src/main/java/com/erdincozdemir/Main.java
+++ JavaProject_002_NotOrtalamasi2/src/main/java/com/erdincozdemir/Main.java
@@ -1,7 +1,30 @@
 package com.erdincozdemir;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Syst
+
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.println("A sayısına bir değper ataması yapiniz : ");
+        a = input.nextInt();
+        System.out.println("A değeri = "+ a);
+
+        System.out.println("--------------");
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("B sayısına double bir değper ataması yapiniz : ");
+        double b = input2.nextDouble();
+        System.out.println("B değeri = "+ b);
+
+        System.out.println("--------------");
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Ekrana yaz bişeyler");
+        String c = input3.nextLine();
+        System.out.println("Ekrana yazdıkların : "+c);
+
+
+
+
     }
 }
