67e7984a9f51eb09880a33d5441ef605efb297f9
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 13:42:15 +0300

    Kdv Hesaplama

--- JavaProject_003_KDV_Hesaplama/src/main/java/com/erdincozdemir/AppMain.java
+++ JavaProject_003_KDV_Hesaplama/src/main/java/com/erdincozdemir/AppMain.java
@@ -1,9 +1,25 @@
 package com.erdincozdemir;
 
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
 
-        System.out.println("Erdinc Trial");
+        Scanner scanner = new Scanner(System.in);
+        double ucret;
+
+        System.out.print("Kdv hesaplanacak ücreti giriniz : ");
+        ucret = scanner.nextDouble();
+
+        System.out.println("Kdv'siz Fiyat : " + ucret);
+
+        double kdvTutari = ucret * 0.18;
+        System.out.println("Kdv Tutari : " + kdvTutari);
+
+        double ucretKdvli = ucret + kdvTutari;
+        System.out.println("Kdv'li Fiyat : " + ucretKdvli);
+
+
 
     }
 }
