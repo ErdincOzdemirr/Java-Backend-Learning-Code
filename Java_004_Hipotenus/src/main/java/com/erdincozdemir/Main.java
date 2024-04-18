654c04d62fe1bf5c5b5960f249799bd5e07d490e
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 16:22:07 +0300

    HipotanüsJava

--- Java_004_Hipotenus/src/main/java/com/erdincozdemir/Main.java
+++ Java_004_Hipotenus/src/main/java/com/erdincozdemir/Main.java
@@ -1,7 +1,21 @@
 package com.erdincozdemir;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I'm Erdinc.");
+
+        int a, b;
+        double c;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dik üçgenin birinci kenarını gir : ");
+        a = scanner.nextInt();
+        System.out.print("Dik üçgenin ikinci kenarını gir : ");
+        b = scanner.nextInt();
+        c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.print("Hipotanüs : "+c);
+
     }
 }
