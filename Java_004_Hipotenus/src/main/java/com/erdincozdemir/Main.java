a49850da6637a6987eeb034682528d8fbdadb371
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 17:06:58 +0300

    JavaTaksimetre

--- Java_004_Hipotenus/src/main/java/com/erdincozdemir/Main.java
+++ Java_004_Hipotenus/src/main/java/com/erdincozdemir/Main.java
@@ -12,8 +12,10 @@ public class Main {
 
         System.out.print("Dik üçgenin birinci kenarını gir : ");
         a = scanner.nextInt();
+
         System.out.print("Dik üçgenin ikinci kenarını gir : ");
         b = scanner.nextInt();
+
         c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
         System.out.print("Hipotanüs : "+c);
 
