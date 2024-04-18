413ab6ae28cc871e78900c6540b235d53cee5020
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-18 11:31:43 +0300

    Not Ortalamasi

--- JavaProject_002_NotOrtalamasi2/src/main/java/com/erdincozdemir/Main.java
+++ JavaProject_002_NotOrtalamasi2/src/main/java/com/erdincozdemir/Main.java
@@ -1,8 +1,41 @@
 package com.erdincozdemir;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+// Değişkenleri oluştur
+        int mat,fiz,kimya,turkce,tarih,muzik;
 
-        System.out.println("Deneme");
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Matematik Notunuzu Giriniz : ");
+        mat = inp.nextInt();
+        System.out.println("Matematik Notunuz : "+ mat);
+
+        System.out.println("fizik Notunuzu Giriniz : ");
+        fiz = inp.nextInt();
+        System.out.println("fizik Notunuz : "+ fiz);
+
+        System.out.println("kimya Notunuzu Giriniz : ");
+        kimya = inp.nextInt();
+        System.out.println("kimya Notunuz : "+ kimya);
+
+        System.out.println("turkce Notunuzu Giriniz : ");
+        turkce = inp.nextInt();
+        System.out.println("turkce Notunuz : "+ turkce);
+
+        System.out.println("tarih Notunuzu Giriniz : ");
+        tarih = inp.nextInt();
+        System.out.println("tarih Notunuz : "+ tarih);
+
+        System.out.println("muzik Notunuzu Giriniz : ");
+        muzik = inp.nextInt();
+        System.out.println("muzik Notunuz : "+ muzik);
+
+        int notToplamlari = (mat+fiz+kimya+turkce+tarih+muzik);
+        double notOrtalamasi = notToplamlari/6.0;
+        System.out.println("notOrtalamasi "+ notOrtalamasi);
+
     }
 }
