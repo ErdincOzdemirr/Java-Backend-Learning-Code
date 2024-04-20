b4ed842ec898c4f9ea1f3cbd17322f2df9059923
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-20 11:48:27 +0300

    operator
    operator

---
+++ Java_009_Oparator/src/main/java/com/erdincozdemir/Main.java
@@ -0,0 +1,13 @@
+package com.erdincozdemir;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        int C = 10;
+        int D = 40;
+
+        System.out.println(A!=B);
+    }
+}
