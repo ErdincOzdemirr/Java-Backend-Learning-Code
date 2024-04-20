8f4e0189737a7c9b5b273416e970e22793c6813e
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-04-20 11:52:11 +0300

    Logic Table
    Logic Table

---
+++ Java_010_Mantik/src/main/java/com/erdincozdemir/Main.java
@@ -0,0 +1,17 @@
+package com.erdincozdemir;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        boolean kosul1 = ( a == b);
+        boolean kosul2 = ( a <= c);
+
+        boolean sonuc = kosul1 && kosul2;
+        System.out.println(sonuc);
+    }
+}
